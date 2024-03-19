648bca2b411a12d66fcd23b6bcc0241d7a65eb8d
myh98 <90823418+Munyounghyun@users.noreply.github.com>
2024-03-20 01:53:58 +0900

    로그인 작성 및 테스트

--- src/main/java/com/dot/tour_info_service_server/mapper/MemberMapper.java
+++ src/main/java/com/dot/tour_info_service_server/mapper/MemberMapper.java
@@ -7,7 +7,11 @@ import java.util.List;
 
 @Mapper
 public interface MemberMapper {
+
+    //로그인하기 위한 유저 정보 들고오기
+    Member getLogin(String email);
+
     //Oauth2 로그인 아닌 회원 email로 찾기
-    public List<Member> getUserProfiles(Long mno);
+    List<Member> getUserProfiles(Long mno);
 
 }
--- src/test/java/com/dot/tour_info_service_server/mapper/MemberMapperTest.java
+++ src/test/java/com/dot/tour_info_service_server/mapper/MemberMapperTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 // 실 데이터베이스에 연결 시 필요한 어노테이션
@@ -15,6 +16,11 @@ class MemberMapperTest {
     @Autowired
     private MemberMapper memberMapper;
 
+    //로그인 테스트
+    @Test
+    public void loginTest(){
+        log.info("로그인 테스트 : "+memberMapper.getLogin("fldh3369@naver.com"));
+    }
     @Test
     public void findMembertest(){
         log.info("테스트 : "+memberMapper.getUserProfiles(1l));
